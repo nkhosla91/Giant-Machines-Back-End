de7f93a89e495c4af15baf4cfae33cdbddcd5a1c
github username <github email address>
2020-01-22 19:06:38 -0500

    functioning backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,25 @@
 require 'rest-client'
 require 'json'
 require 'csv'
+
+Work.destroy_all
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', '../../GM_Coding_Exercise_Sample_Data.csv'))
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+    Work.create ({
+        date: row["Date"],
+        client: row["Client"],
+        project: row["Project"],
+        projectCode: row["Project Code"],
+        hours: row["Hours"],
+        hoursRounded: row["Hours Rounded"],
+        billable: row["Billable?"],
+        firstName: row["First Name"],
+        lastName: row["Last Name"],
+        billableRate: row["Billable Rate"]
+    })
+end
+
+# Work.first.destroy
+    
